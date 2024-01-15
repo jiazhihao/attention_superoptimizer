2dd333c15f39c6ca19ffd9ac3c5603ca2def6d18
Zhihao Jia <zhihao@cs.stanford.edu>
2024-01-15 09:28:30 -0700

    .

--- src/threadblock/cuda/matmul.cu
+++ src/threadblock/cuda/matmul.cu
@@ -28,10 +28,49 @@ template<
     typename scalar_type,
     typename arch_tag>
 
-
-static void CUTLASS_DEVICE Operator::compute_kernel(void) {
+void CUTLASS_DEVICE Operator::compute_kernel(void) {
   extern __shared__ char smem_buffer[];
+  WarpLoadedFragmentA warp_loaded_frag_A[2];
+  WarpLoadedFragmentB warp_loaded_frag_B[2];
+  FragmentC accum;
+  WarpTransformedFragmentA warp_transformed_frag_A[2];
+  WarpTransformedFragmentB warp_transformed_frag_B[2];
+  Epilogue epilogue;
 
+  WarpOperator warp_mma;
+
+  warp_tile_iterator_A.set_kgroup_index(0);
+  warp_tile_iterator_B.set_kgroup_index(0);
+  warp_tile_iterator_A.load(warp_loaded_frag_A[0]);
+  warp_tile_iterator_B.load(warp_loaded_frag_B[0]);
+  ++warp_tile_iterator_A;
+  ++warp_tile_iterator_B;
+
+  int gemm_k_iterations = ;
+  CUTLASS_GEMM_LOOP
+  for (int warp_mma_k = 0; warp_mma_k < kWarpGemmIterations; warp_mma_k++) {
+    // Load warp-level tiles from shared memory, wrapping to k offset if
+    // this is the last group as the case may be.
+    warp_tile_iterator_A.set_kgroup_index((warp_mma_k + 1) % kWarpGemmIterations);
+    warp_tile_iterator_B.set_kgroup_index((warp_mma_k + 1) % kWarpGemmIterations);
+    warp_tile_iterator_A.load(warp_loaded_frag_A[(warp_mma_k + 1) % 2]);
+    warp_tile_iterator_B.load(warp_loaded_frag_B[(warp_mma_k + 1) % 2]);
+    ++warp_tile_iterator_A;
+    ++warp_tile_iterator_B;
+    iterator_A.clear_mask(gemm_k_iterations_0 == 0);
+    warp_mma.transform(warp_transformed_frag_A[warp_mma_k % 2],
+                       warp_transformed_frag_B[warp_mma_k % 2],
+                       warp_loaded_frag_A[warp_mma_k % 2],
+                       warp_loaded_frag_B[warp_mma_k % 2]);
+    warp_mma(
+      accum,
+      warp_transformed_frag_A[warp_mma_k % 2],
+      warp_transformed_frag_B[warp_mma_k % 2],
+      accum
+    );
+  }
+  epilogue(output_op, smem_iterator_D, accum);
+  __syncthreads();
 }
 
 } // namespace matmul
