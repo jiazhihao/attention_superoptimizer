a0df6ec3adde4ce82c1c50a0f4f2b631563283e2
Zhihao Jia <zhihao@cmu.edu>
2024-02-04 22:47:50 -0500

    add fingerprinter

--- include/aso/threadblock/cuda/reduction.h
+++ include/aso/threadblock/cuda/reduction.h
@@ -22,6 +22,7 @@ namespace aso {
 namespace threadblock {
 
 using namespace cutlass;
+using namespace aso::type;
 
 template <typename ElementType>
 class RedunctionExecutor {
@@ -34,6 +35,59 @@ public:
   void CUTLASS_DEVICE execute_kernel(void) {
     assert(false && "To Be Implemented");
   }
+};
+
+class TBReductionFingerprinter {
+public:
+  CUTLASS_DEVICE
+  TBReductionFingerprinter(aso::type::TBOperatorType type,
+                           char *smem_buffer,
+                           STensor const &input,
+                           STensor const &output,
+                           int thread_id,
+                           int num_threads) {
+    FPType *input_ptr = (FPType *)(smem_buffer + input.smem_offset);
+    FPType *output_ptr = (FPType *)(smem_buffer + output.smem_offset);
+    int num_elements = output.size();
+    if (type == TB_REDUCTION_0_OP) {
+      int kK = input.dim[0] / output.dim[0];
+      int kIterations = (num_elements + num_threads - 1) / num_threads;
+      for (int i = 0; i < kIterations; i++) {
+        uint32_t result = 0;
+        int no = (i * num_threads + thread_id) / output.dim[1];
+        int m = (i * num_threads + thread_id) % output.dim[1];
+        if (no >= output.dim[0]) {
+          continue;
+        }
+        for (int k = 0; k < kK; k++) {
+          int ni = no + k * output.dim[0];
+          result =
+              (result + input_ptr[ni * input.stride[0] + m * input.stride[1]]) %
+              FP_PQ;
+        }
+        output_ptr[no * output.stride[0] + m * output.stride[1]] = result;
+      }
+    } else if (type == TB_REDUCTION_1_OP) {
+      int kK = input.dim[1] / output.dim[1];
+      for (int i = 0; i < (num_elements + num_threads - 1) / num_threads; i++) {
+        uint32_t result = 0;
+        int n = (i * num_threads + thread_id) / output.dim[1];
+        int mo = (i * num_threads + thread_id) % output.dim[1];
+        if (n >= output.dim[0]) {
+          continue;
+        }
+        for (int k = 0; k < kK; k++) {
+          int mi = mo + k * output.dim[1];
+          result =
+              (result + input_ptr[n * input.stride[0] + mi * input.stride[1]]) %
+              FP_PQ;
+        }
+        output_ptr[n * output.stride[0] + mo * output.stride[1]] = result;
+      }
+    } else {
+      assert(false && "Unimplemented");
+    }
+  };
 };
 
 } // namespace threadblock
